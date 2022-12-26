5492e737096d20d4787362b0228ac7f4e7ef3061
Taha Toy <mttoy45@gmail.com>
2022-12-26 16:30:12 +0300

    init

--- BulkyBookWeb/wwwroot/js/site.js
+++ BulkyBookWeb/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+$('nav a-profile').click(function (e) {
+    e.preventDefault();
+    $('nav a-profile').removeClass('active');
+    $(this).addClass('active');
+    if (this.id === !'payment') {
+        $('.payment').addClass('noshow');
+    }
+    else if (this.id === 'payment') {
+        $('.payment').removeClass('noshow');
+        $('.rightbox').children().not('.payment').addClass('noshow');
+    }
+    else if (this.id === 'profile') {
+        $('.profile').removeClass('noshow');
+        $('.rightbox').children().not('.profile').addClass('noshow');
+    }
+    else if (this.id === 'subscription') {
+        $('.subscription').removeClass('noshow');
+        $('.rightbox').children().not('.subscription').addClass('noshow');
+    }
+    else if (this.id === 'privacy') {
+        $('.privacy').removeClass('noshow');
+        $('.rightbox').children().not('.privacy').addClass('noshow');
+    }
+    else if (this.id === 'settings') {
+        $('.settings').removeClass('noshow');
+        $('.rightbox').children().not('.settings').addClass('noshow');
+    }
+});
