5d3387de53f63a8c0afa4ade370e468f4ee974a5
oguzhannx <oguzhanoztrk00@gmail.com>
2022-12-27 14:32:12 +0300

    profile bug düzeltildi, home index eklendi,

---
+++ BulkBook.DataAccess/Migrations/20221227110413_deneme43453.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkBook.DataAccess.Migrations
+{
+    public partial class deneme43453 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
